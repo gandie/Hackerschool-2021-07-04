d942c1100d60014cb58bdc10668768efc91c3376
Lars Bergmann <lars@bergmann82.de>
2021-07-04 11:33:06 +0000

    Allow calling the program from command line

--- main.py
+++ main.py
@@ -1,50 +1,67 @@
+import sys
 import os
-
-# https://docs.python.org/3/library/os.path.html?highlight=os%20path#module-os.path
-from os.path import join
-
-# STUFE 1 - Wir suchen alle Dateien zusammen
-
-# Hier sammeln wir unsere Dateien
-file_paths = []
-
-# wir laufen durch alle ordner an unserem aktuellen ort
-# damit ist der punkt gemeint!
-for root, dirs, files in os.walk('.'):
-    # das aktuelle verzeichnis ausgeben, also root
-    # print(root, "consumes", end=" ")
-    #print("Wir sind gerade in " + root)
-    #print("Hier liegen diese Dateien: " + str(files))
-    #print("Hier gibt es diese Ordner: " + str(dirs))
-
-    # Innerer Loop: Wir sammeln Dateien in einem Ordner
-    for file_name in files:
-        #print("Hier liegt Datei: " + file_name)
-        file_path = join(root, file_name)
-        #print("Der Pfad lautet: " + file_path)
-        file_paths.append(file_path)
-
-    #print("__________________")
-    #print()
-
-#print("Ergebnis Stufe 1")
-#print(file_paths)
-
-# STUFE 2 - Unteruschen auf gleiche Dateien
 
 # https://docs.python.org/3/library/itertools.html
 import itertools
 
-# Idee: Zwei Dateien sind dann gleich, wenn alle Bytes der
-# Datei gleich sind
+# https://docs.python.org/3/library/os.path.html?highlight=os%20path#module-os.path
+from os.path import join
 
-for combination in itertools.combinations(file_paths, 2):
-    # print(combination)
-    path_one, path_two = combination
 
-    handle_one = open(path_one, 'r')
-    handle_two = open(path_two, 'r')
+'''
+- 2. ) Wie mit bearbeiteten Bildern umgehen?
+- 1. ) Wie auf andere Orte anwenden? 
+'''
 
-    if handle_one.read() == handle_two.read():
-        print("Dublette gefunden!")
-        print(combination)
+def find_files(start_path):
+    '''
+    STUFE 1 - Wir suchen alle Dateien zusammen
+
+    Argumente:
+    
+    start_path : Der Ort im Dateisystem, ab dem das Programm laufen soll
+    '''
+
+    # Hier sammeln wir unsere Dateien
+    file_paths = []
+
+    # wir laufen durch alle ordner an unserem
+    # eingegebenen start_path
+    for root, dirs, files in os.walk(start_path):
+
+        # Innerer Loop: Wir sammeln Dateien in einem Ordner
+        for file_name in files:
+            #print("Hier liegt Datei: " + file_name)
+            file_path = join(root, file_name)
+            #print("Der Pfad lautet: " + file_path)
+            file_paths.append(file_path)
+
+    return file_paths
+
+def check_double_files(files):
+    '''
+    STUFE 2 - Unteruschen auf gleiche Dateien
+
+    Idee: Zwei Dateien sind dann gleich, wenn alle Bytes der Datei gleich sind
+
+    Argumente:
+    files: Liste mit Dateien, die geprüft werden sollen
+    '''
+
+    for combination in itertools.combinations(files, 2):
+        # print(combination)
+        path_one, path_two = combination
+
+        handle_one = open(path_one, 'rb')
+        handle_two = open(path_two, 'rb')
+
+        if handle_one.read() == handle_two.read():
+            print("Dublette gefunden!")
+            print(combination)
+
+
+if __name__ == '__main__':
+    start = sys.argv[1]
+    print(start)
+    files = find_files(start)
+    check_double_files(files)
